3ccb68ba0f8c3327cb758ff517a1ee774d5e7895
Sarunas <sarunasdov4@gmail.com>
2023-09-25 21:06:24 +0300

    Android project button and textView

--- app/src/main/java/com/example/a1_mipt/MainActivity.java
+++ app/src/main/java/com/example/a1_mipt/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.a1_mipt;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize views
+        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+
+        // Set click listener for the button
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                textView.setText("Laba diena!! ");
+            }
+        });
     }
 }
